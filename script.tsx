5023ea1e90a641a003cc78b27e6335c02ce74f49
Alen <alen@anon>
2021-06-27 07:12:35 +0100

    merge input into script

--- script.tsx
+++ script.tsx
@@ -235,6 +235,38 @@ const kinks = {
 let colors = {}
 let level = {};
 
+const updateOwner = (target: HTMLInputElement) => {
+    // Update title
+    if (target.value.length > 0) {
+        document.title = `${target.value}'s Kink List`;
+        localStorage.owner = target.value;
+    } else {
+        document.title = 'Kink List';
+        localStorage.removeItem('owner');
+    }
+
+    // Update input size
+    const content = target.value || target.placeholder;
+    const span = document.createElement("span");
+    span.innerHTML = content
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+    target.parentElement.appendChild(span);
+    const theWidth = span.getBoundingClientRect().width;
+    target.parentElement.removeChild(span);
+    target.style.width = `${theWidth}px`;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const nameInput = document.getElementById('name') as HTMLInputElement;
+    if (localStorage.owner !== undefined) {
+        nameInput.value = localStorage.owner;
+    }
+    updateOwner(nameInput);
+    nameInput.addEventListener('input', (event: InputEvent) => updateOwner(event.target as HTMLInputElement));
+});
+
 const setupDOM = () => {
     const inputList = $('#InputList');
     inputList.empty();
